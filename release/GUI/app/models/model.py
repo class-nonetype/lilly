fbfd2950db58328bf4597641e1de36afbbf104c5
GMVZ <go.vivanco@duocuc.cl>
2023-02-10 08:23:59 -0300

    Lilly version 2.2

---
+++ release/GUI/app/models/model.py
@@ -0,0 +1,37 @@
+
+
+from app.models.data import DataModel
+from app.models.json_tree import JsonModel
+
+from PyQt5 import (
+    QtWidgets)
+
+import os
+import logging
+
+
+
+
+
+class Model(object):
+    
+    def __init__(self, Controller, logger : logging.Logger) -> None:
+        self.Controller = Controller
+
+        self.logger = logger
+
+        self.DataModel = DataModel(self.logger)
+
+        
+        
+        self.FileSystemModel = QtWidgets.QFileSystemModel()
+        self.FileSystemModel.setRootPath('')
+        
+        self.JsonModel = JsonModel()
+        
+
+    def set_struct(self, struct):
+        self.struct = struct
+        
+        return self.struct
+
